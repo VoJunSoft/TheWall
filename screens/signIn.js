40120784ccd72d17bf52ae425ffe78b364295a57
khaled <thenonexist@hotmail.com>
2020-07-21 20:32:02 +0300

    init folders

---
+++ screens/signIn.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const signIn = () => {
+    <View>
+              <Text style={styles.title}>The Wall</Text>
+    </View>
+
+}
+
+const styles = StyleSheet.create({
+    title: {
+      fontSize: 24,
+      fontWeight: '600',
+      color: Colors.purple,
+      fontSize: 12,
+      fontWeight: '600',
+      padding: 4,
+      paddingRight: 12,
+      textAlign: 'right',
+    },
+
+  });
+export default signIn;
